31362ebaba36a931115edf794074e67cb5164ea3
sexta13 <sexta13@mbp-de-sergio.home>
2013-10-31 02:09:52 +0000

    pesquisa em todas as pags

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,11 @@
 class ProductsController < ApplicationController
   def view_details
-    @product = Product.find(params[:id])
+    gon.projects=session[:projects]
 
+    @product = Product.find(params[:id])    
     @twitter = Twitter::Client.new
     @twitter = @twitter.search(@product.hashtag, :include_entities=>"t", :count => 6, :result_type => "recent")
+    
     gon.twitter=@twitter
     gon.hashtag=@product.hashtag
     gon.region = @product.region
@@ -11,8 +13,10 @@ class ProductsController < ApplicationController
 
   def prods_by_category
     gon.projects=session[:projects]
+    
     @products = Product.where(category_id: params[:id])	   
     @category_name= @products.first.name       
+    
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @products }
