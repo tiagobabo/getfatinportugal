931b9a1834a8342be5d4dfdcc036dbb830441e3b
sexta13 <sexta.feira.13@gmail.com>
2013-11-04 18:01:56 +0000

    google maps

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -5,6 +5,9 @@
 //= require admin/products
 //= require admin/categories
 
+document.write("<script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyCg38ejoS6k0WzzLIBM39H8gED-jUwDFrw&sensor=true' type='text/javascript'></script>");
+
 $(function() {
    $(document).foundation();
 });
+
--- app/assets/javascripts/admin/products.js
+++ app/assets/javascripts/admin/products.js
@@ -1,3 +1,4 @@
+
 function get_product_categories_content(url,element)
 {
 	set_active_class(element);
@@ -20,3 +21,52 @@ function set_active_class(element)
 	$(element).parent().addClass("active");
 
 }
+
+	
+  var markersArray = [];	
+	 function codeAddress() {
+        address = $("#map_search").val();
+        geocoder = new google.maps.Geocoder();
+        geocoder.geocode({
+            'address': address
+        }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                var myOptions = {
+                    zoom: 12,
+                    center: results[0].geometry.location,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                }
+                map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+
+
+                google.maps.event.addListener(map, 'click', function (event) {
+
+                    $(product_longitude).val(event.latLng.lng());
+                    $(product_latitude).val(event.latLng.lat())
+                    //alert(event.latLng);
+                    placeMarker(event.latLng);
+                });
+
+
+            }
+        });
+    }
+	
+	    function placeMarker(location) {
+        clearOverlays();
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+        });
+        markersArray.push(marker);
+
+
+    }
+	
+	 function clearOverlays() {
+        for (var i = 0; i < markersArray.length; i++) {
+            markersArray[i].setMap(null);
+        }
+        markersArray = [];
+    }
+	
