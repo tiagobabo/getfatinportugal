ea71b83ee36f02486919d06d0253b46d7e0c23ba
sexta13 <sexta.feira.13@gmail.com>
2013-11-04 15:40:02 +0000

    email
    email contact us

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -8,6 +8,10 @@ class ContactController < ApplicationController
 	
 	def sendemail	
 		Emailer.contact_us(params[:name], params[:email], params[:subject], params[:message]).deliver
+		
+		 respond_to do |format|
+			format.html { redirect_to main_index_path, notice: 'Email was successfully sent.' }     
+	 	 end
 	end
 
 end
