752d58f676031e1c70d8cad4e4c84c33bb1b3b87
sexta13 <sexta13@mbp-de-sergio.home>
2013-11-08 23:37:47 +0000

    social

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,3 +1,4 @@
+document.write("<script async src='http://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js' type='text/javascript'></script>");
 $(document).ready(function () {
    var googleMaps = new GoogleMaps('map-canvas');
    googleMaps.zoom=10;   
@@ -10,7 +11,10 @@ $(document).ready(function () {
    else {	   
 	google.maps.event.addDomListener($("#map-canvas"), 'load', googleMaps.initializeCoordinates(gon.latitude, gon.longitude));
    }
+//google ads
+(adsbygoogle = window.adsbygoogle || []).push({});
 
+//facebook
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
@@ -19,6 +23,7 @@ $(document).ready(function () {
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
+// twitter
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 });
 
