11ea4f30b49a5a301d4ea1aa52bef26281094a16
sexta13 <sexta13@mbp-de-sergio.home>
2013-11-13 23:29:53 +0000

    clients

--- app/controllers/admin/clients_controller.rb
+++ app/controllers/admin/clients_controller.rb
@@ -1,33 +1,33 @@
 class Admin::ClientsController < ApplicationController
-before_filter :authenticate_user!
-
-def index
-
+  layout 'admin'
+  before_filter :authenticate_user!
+  
+  def index
+          
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  def show
+    
+  end
+  
 end
-
-def show
-
-end
-
-def create 
-
-end
-
-def new 
-
-end
-
-def destroy
-
-end
-
-def update
-
-end
-
-def edit
-
-end
-
-end
-
