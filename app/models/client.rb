773413fd962c2c9b6bab1c9e8a0ac8deabf472ac
sexta13 <sexta.feira.13@gmail.com>
2013-11-14 18:35:04 +0000

    a

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,5 @@
 class Client < ActiveRecord::Base
  belongs_to :client_type
+ 
   attr_accessible :address, :email, :location, :mobile_phone, :name, :nif, :person_in_charge, :phone, :postal_code, :client_type_id
 end
