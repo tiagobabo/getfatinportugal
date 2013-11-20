bfba91febbb782808ab536d1bc08066fec0c7837
sexta13 <sexta13@mbp-de-sergio.home>
2013-11-20 23:36:40 +0000

    product details

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+   var googleMaps = new GoogleMaps('map-canvas');
+   googleMaps.zoom=10;   
+   googleMaps.mapType = google.maps.MapTypeId.ROADMAP;
+  
+  if(!gon.latitude && !gon.longitude)
+   {	   
+	google.maps.event.addDomListener($("#map-canvas"), 'load', googleMaps.initializeWithRegion(gon.region));
+   }
+   else {	   
+	google.maps.event.addDomListener($("#map-canvas"), 'load', googleMaps.initializeCoordinates(gon.latitude, gon.longitude, true));
+   }
+});
