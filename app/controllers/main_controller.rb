0c8ddb4f06a107b407f3a05ea17b4c3d5188a2f6
sexta13 <sexta13@mbp-de-sergio.home>
2013-11-20 02:30:37 +0000

    faq

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,7 +7,7 @@ class MainController < ApplicationController
   
   def faq
     products_all = Product.where(is_active: 1) 
-    @products_recent = products_all.order(created_at: :desc).first(4)
+    @products_recent = products_all.order('created_at DESC').limit(4)
     @products_random = products_all.sample(4) 
   end
   
