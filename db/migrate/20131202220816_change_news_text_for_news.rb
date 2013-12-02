a8719af1027892a9961e71158aaf758f7c73dd57
sexta13 <sexta13@mbp-de-sergio.home>
2013-12-02 23:17:49 +0000

    limit

--- db/migrate/20131202220816_change_news_text_for_news.rb
+++ db/migrate/20131202220816_change_news_text_for_news.rb
@@ -1,5 +1,5 @@
 class ChangeNewsTextForNews < ActiveRecord::Migration
   change_table :news do |t|  
-          t.change :news_text, :text , :limit => 4294967295
+          t.change :news_text, :text , :limit => 16777215
       end
 end
