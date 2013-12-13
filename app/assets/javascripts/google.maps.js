c4033fdf4e9ef6fc79e0a7659dd7f7a51dda161f
sexta13 <sexta13@mbp-de-sergio.home>
2013-12-13 01:12:05 +0000

    google maps

--- app/assets/javascripts/google.maps.js
+++ app/assets/javascripts/google.maps.js
@@ -116,7 +116,7 @@ GoogleMaps.prototype.showMapByAddress = function(address, longitude, latitude)
 GoogleMaps.prototype.showCountries = function(countries)
 {	
 	var _this = this;	
-	geocoder = new google.maps.Geocoder();
+
 
 	var myOptions = {
 		zoom: _this.zoom,
@@ -124,46 +124,45 @@ GoogleMaps.prototype.showCountries = function(countries)
 		mapTypeId:_this.mapType
 	}
 	_this.map = new google.maps.Map(document.getElementById(_this.mapCanvas), myOptions);
-	var counter = 0;
+var counter=0;
 	countries.forEach(function(entry) 
 	{
-		geocoder.geocode({
-			'address': entry.name
-		}, function (results, status) {
-			if (status == google.maps.GeocoderStatus.OK) {
+		counter++;
+		setTimeout(_this.getAddress(entry),150*counter)
+	});
+}
+GoogleMaps.prototype.getAddress = function (entry)
+{
+	var _this = this;	
+	var geocoder = new google.maps.Geocoder();
+	geocoder.geocode({
+		'address': entry.name
+	}, function (results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
 
-				var myLatlng = new google.maps.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng());
-				_this.latitude= results[0].geometry.location.lat();
-				_this.longitude = results[0].geometry.location.lng();
+			var myLatlng = new google.maps.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng());
+			_this.latitude= results[0].geometry.location.lat();
+			_this.longitude = results[0].geometry.location.lng();
 
+			_this.placeMarkerWithLabel(entry.name, myLatlng,entry.slug );   
 
-				var	timer = setInterval(function () {
-					_this.placeMarkerWithLabel(entry.name, myLatlng,entry.slug );  
-					counter++
-					if (counter === countries.length) {
-						clearInterval(timer);
-					}
-					}, 1000);
+		}
+	});
+	
+}
+GoogleMaps.prototype.placeMarkerWithLabel = function(countryName,latlng,linkCountry)
+{
+	var _this = this;
+	var marker = new MarkerWithLabel({
+		position: latlng,
+		draggable: false,
+		map: _this.map,      
+	});
 
+	var iw = new google.maps.InfoWindow({
+		content: '<a href="/portuguese/'+linkCountry+'">Portuguese restaurants in '+countryName+'</a>'
+	});
 
-
-				}
-			});
-		});
-	}
-	GoogleMaps.prototype.placeMarkerWithLabel = function(countryName,latlng,linkCountry)
-	{
-		var _this = this;
-		var marker = new MarkerWithLabel({
-			position: latlng,
-			draggable: false,
-			map: _this.map,      
-		});
-
-		var iw = new google.maps.InfoWindow({
-			content: '<a href="/portuguese/'+linkCountry+'">Portuguese restaurants in '+countryName+'</a>'
-		});
-
-		this.markersArray.push(marker);
-		google.maps.event.addListener(marker, "click", function (e) { iw.open(_this.map, marker); });
-	}
+	this.markersArray.push(marker);
+	google.maps.event.addListener(marker, "click", function (e) { iw.open(_this.map, marker); });
+}
