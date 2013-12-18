ff5be2be9e23605b1b850cf7b1c99767afcc2c04
sexta13 <sexta13@mbp-de-sergio.home>
2013-12-18 00:24:28 +0000

    changes

--- app/assets/javascripts/client.js
+++ app/assets/javascripts/client.js
@@ -6,4 +6,7 @@ $(document).ready(function () {
 	   
 	google.maps.event.addDomListener($("#map-canvas"), 'load', googleMaps.showMapByAddress(gon.address,null,null));
   
+	//twitter
+	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
 });
