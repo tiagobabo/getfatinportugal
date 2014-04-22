6c4f62c6de8b36ac9b28e2cb3cebe271370b911c
sexta13 <sexta.feira.13@gmail.com>
2014-04-22 17:35:36 +0100

    js para dar o google maps

--- app/assets/javascripts/google.maps.js
+++ app/assets/javascripts/google.maps.js
@@ -130,6 +130,8 @@ GoogleMaps.prototype.showCountries = function(countries)
 	var myLatlng = new google.maps.LatLng(38.69714979357789 , -9.205266237258911);
 	_this.placeMarkerWithLabel(myLatlng,"There's no place like home :)" );  
 
+	
+
 	countries.forEach(function(entry) 
 	{
 		if(entry.latitude && entry.longitude)
@@ -167,16 +169,16 @@ GoogleMaps.prototype.showCountries = function(countries)
 					});
 				}
 			});
+	
 		}
 
 GoogleMaps.prototype.placeMarkerWithLabel = function(latlng,content)
 {
 			var _this = this;
-			var marker = new MarkerWithLabel({
-				position: latlng,
-				draggable: false,
-				map: _this.map,      
-			});
+			var marker = new google.maps.Marker({
+		position: latlng,
+		map: _this.map,
+	});
 
 			var iw = new google.maps.InfoWindow({
 				content: content
