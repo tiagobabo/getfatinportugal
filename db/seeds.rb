1e57ca2c1179a9c9886a79a7dc2ea8bae25548a3
lbramos <luis.bivar@gmail.com>
2014-11-11 23:00:49 +0000

    removes some data from the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,3 @@
 ["Drinks", "Desserts", "Main Course", "Starters"].each do |cat|
   Category.find_or_create_by_name(cat)
 end
-
-
-  User.find_or_create_by_email(:email => 'sexta.feira.13@gmail.com', :remember_me => 0, :password => 'qwerty12', :password_confirmation => 'qwerty12', :first_name => 'Sergio', :last_name => 'Monteiro', :nickname => 'sexta13')
-  User.find_or_create_by_email(:email => 'luis.bivar@gmail.com', :remember_me => 0, :password => 'qwerty12', :password_confirmation => 'qwerty12', :first_name => 'Luis', :last_name => 'Ramos', :nickname => 'lbramos')
-  User.find_or_create_by_email( :email => 'rsneves@gmail.com', :remember_me => 0, :password => 'qwerty12', :password_confirmation => 'qwerty12', :first_name => 'Rui', :last_name => 'Neves', :nickname => 'rsneves')
